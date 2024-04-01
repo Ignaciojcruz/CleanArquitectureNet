dc793b976acfb98c693094e7b2e5dd198aaa7224
Ignacio Cruz <ignaciojcruz@hotmail.com>
2024-03-31 21:29:41 -0300

    Capa Infrastructure - Add Email service

---
+++ CleanArquitecture.Data/InfrastructureServiceRegistration.cs
@@ -0,0 +1,32 @@
+﻿using CleanArquitectureNet.Application.Contracts.Infrastructure;
+using CleanArquitectureNet.Application.Contracts.Persistence;
+using CleanArquitectureNet.Application.Models;
+using CleanArquitectureNet.Infrastructure.Email;
+using CleanArquitectureNet.Infrastructure.Persistence;
+using CleanArquitectureNet.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArquitectureNet.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services
+                                                                        , IConfiguration configuration) 
+        {
+            services.AddDbContext<StreamerDbContext>(option => 
+                option.UseSqlServer(configuration.GetConnectionString("ConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IVideoRepository, VideoRepository>();
+            services.AddScoped<IStreamerRepository, StreamerRepository>();
+
+            services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+
+}
