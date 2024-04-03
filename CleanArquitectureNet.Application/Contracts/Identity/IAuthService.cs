0ade9303dc3001d36530b5fca8ceb756f0212e4a
Ignacio Cruz <ignaciojcruz@hotmail.com>
2024-04-02 21:39:23 -0300

    Seguridad en ASP.NET Clean Arquitecture - Add Identity

---
+++ CleanArquitectureNet.Application/Contracts/Identity/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using CleanArquitectureNet.Application.Models.Identity;
+
+namespace CleanArquitectureNet.Application.Contracts.Identity
+{
+    public interface IAuthService
+    {
+        Task<AuthResponse> Login(AuthRequest request);
+        Task<RegistrationResponse> Register(RegistrationRequest request);
+        
+    }
+}
---
+++ CleanArquitectureNet.Application/Models/Identity/RegistrationRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArquitectureNet.Application.Models.Identity
+{
+    public class RegistrationRequest
+    {
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
